58354ac66f351441d75e0b63a9807e3ee7702668
ruchika <ruchika6698@gmail.com>
2020-03-21 17:38:32 +0530

    Function to check next move in Game

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -3,8 +3,36 @@
 echo "--------------Welcome to Snake Ladder Game--------------"
 
 START_POSITION=0
+NOMOVE=0
+LADDER=2
+SNAKE=1
 
-
-function Die(){
+function Die()
+{
 	Number=$(( 1+(RANDOM%6) ))
+	echo "$Number"
 }
+
+
+function nextmove()
+{
+	a=$((RANDOM%3))
+
+	if [ $a -eq $NOMOVE ]
+	then
+		echo "$CurrentPosition"
+	fi
+
+	if [ $a -eq $SNAKE ]
+	then
+		echo "$(( $CurrentPosition - $1 ))"
+	fi
+
+	if [ $a -eq $LADDER ]
+	then
+		echo "$(( $CurrentPostion + $1 ))"
+	fi
+}
+
+CurrentPosition=$START_POSITION
+CurrentPOsition=$( nextmove $( Die ) )
