d4d36a9451a862d74cc7df7b868db62cdd4904b3
ruchika <ruchika6698@gmail.com>
2020-03-21 19:23:46 +0530

    Added:Check No play,Ladder and snake position

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -3,8 +3,27 @@
 echo "--------------Welcome to Snake Ladder--------------"
 
 START_POSITION=0
+NOPLAY=1
+SNAKE=3
+LADDER=2
 
 Number=$(( RANDOM%6 + 1 ))
 echo $Number
+
+Random=$(( RANDOM%3 + 1 ))
+CurrentPosition=$START_POSITION
+if [[ $Random -eq 1 ]]
+then
+	CurrentPosition=$START_POSITION
+elif [[ $Random -eq 2 ]]
+then
+	CurrentPosition=$(( $CurrentPosition+$Number ))
+elif [[ $Random -eq 3 ]]
+then
+	CurrentPosition=$(( $CurrentPosition-$Number ))
+else
+	echo "wrong input"
+fi
+done
 
 
