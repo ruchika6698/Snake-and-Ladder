0e0f65ae8217eab6a4ef2250718136a3cfbff505
ruchika <ruchika6698@gmail.com>
2020-03-21 20:47:40 +0530

    Added:Display Winner

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -8,29 +8,56 @@ WINNINGPOSITION=100
 SNAKE=3
 LADDER=2
 count=0
+chance=0
 
 Number=$(( RANDOM%6 + 1 ))
 echo $Number
 
-CurrentPosition=$START_POSITION
+CurrentPosition1=$START_POSITION
+CurrentPosition2=$START_POSITION
 
-while [[ CurrentPosition -le $WINNINGPOSITION ]]
+while [[ CurrentPosition1 -le $WINNINGPOSITION && CurrentPosition2 -le $WINNINGPOSITION ]]
 do
 	(( count++ ))
 	Random=$(( RANDOM%3 + 1 ))
-	if [[ $Random -eq 1 ]]
+
+	if [[ chance -eq 0 ]]
 	then
-		CurrentPosition=$CurrentPosition
-	elif [[ $Random -eq 2 ]]
+		if [[ $Random -eq 1 ]]
+		then
+			CurrentPosition1=$CurrentPosition1
+		elif [[ $Random -eq 2 ]]
+		then
+			CurrentPosition1=$(( $CurrentPosition1+$Number ))
+		elif [[ $Random -eq 3 ]]
+		then
+			CurrentPosition1=$(( $CurrentPosition1-$Number ))
+		fi
+	elif [[ chance -eq 1 ]]
 	then
-		CurrentPosition=$(( $CurrentPosition+$Number ))
-	elif [[ $Random -eq 3 ]]
-	then
-		CurrentPosition=$(( $CurrentPosition-$Number ))
-	else
-	echo "wrong input"
+		if [[ $Random -eq 1 ]]
+		then
+			CurrentPosition2=$CurrentPosition2
+		elif [[ $Random -eq 2 ]]
+		then
+			CurrentPosition2=$(( $CurrentPosition2+$Number ))
+		elif [[ $Random -eq 3 ]]
+		then
+			CurrentPosition2=$(( $CurrentPosition2-$Number ))
+		fi
 	fi
 done
 
-echo "Player has reached the winning goal: " $CurrentPosition
+echo "Player has reached the winning goal: " $CurrentPosition1
+echo "Player has reached the winning goal: " $CurrentPosition2
 echo "Number of times the dice was played: " $count
+
+if [[ $CurrentPosition1 -eq 100 ]]
+then
+	echo "Player 1 has won the game"
+elif [[ $CurrentPosition2 -eq 100 ]]
+then
+	echo "Player 2 has won the game"
+else
+	echo "Its a tie"
+fi
