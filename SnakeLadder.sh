1661ab8eb9781f201e2ef8bca3c7023ccf7f2793
ruchika <ruchika6698@gmail.com>
2020-03-21 18:59:48 +0530

    Added:Snake and Ladder Game

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -x
 
-echo "--------------Welcome to Snake Ladder Game--------------"
+echo "--------------Welcome to Snake Ladder--------------"
 
 START_POSITION=0
 NOMOVE=0
 LADDER=2
 SNAKE=1
+WINNINGPOSITION=100
+count=0
+die=0
+count1=0
+
+declare -a Position1
+declare -a Position2
 
 function Die()
 {
@@ -17,22 +24,57 @@ function Die()
 function nextmove()
 {
 	a=$((RANDOM%3))
-
 	if [ $a -eq $NOMOVE ]
 	then
-		echo "$CurrentPosition"
+		echo "$2"
 	fi
 
 	if [ $a -eq $SNAKE ]
 	then
-		echo "$(( $CurrentPosition - $1 ))"
+		echo $(( $2-$1 ))
 	fi
 
 	if [ $a -eq $LADDER ]
 	then
-		echo "$(( $CurrentPostion + $1 ))"
+		echo $(( $2+$1 ))
 	fi
 }
 
-CurrentPosition=$START_POSITION
-CurrentPOsition=$( nextmove $( Die ) )
+function position(){
+	Position1=$( nextmove $( Die $Number ) $CurrentPosition )
+	(( die++ ))
+   if [[ $Position -gt $WINNINGPOSITION ]]
+   then
+      CurrentPosition=$CurrentPosition
+   else
+      CurrentPosition=$Position
+   fi
+
+   if [[ $CurrentPosition -lt $START_POSITION ]]
+   then
+      CurrentPosition=$START_POSITION
+   fi
+	echo "$CurrentPossition"
+}
+
+CurrentPosition1=$START_POSITION
+CurrentPosition2=$START_POSITION
+
+while true
+do
+	PlayerPosition1[count]=$CureentPosition
+   (( count++ ))
+	PlayerPosition2[count1]=$CureentPosition
+	(( count1++ ))
+
+	if [[ $CurrentPostion1 -eq $WINNINGPOSITION ]]
+	then
+		echo "Winner Player 1"
+		break
+	fi
+	if [[ $CurrentPostion2 -eq $WINNINGPOSITION ]]
+   then
+      echo "Winner Player 2"
+      break
+   fi
+done
