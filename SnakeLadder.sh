d32952175e4ee867b12080136ee63d0071c7d0ee
ruchika <ruchika6698@gmail.com>
2020-03-21 19:29:48 +0530

    Added:Repeat till get Winning Position

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -4,26 +4,29 @@ echo "--------------Welcome to Snake Ladder--------------"
 
 START_POSITION=0
 NOPLAY=1
+WINNINGPOSITION=100
 SNAKE=3
 LADDER=2
 
 Number=$(( RANDOM%6 + 1 ))
 echo $Number
 
-Random=$(( RANDOM%3 + 1 ))
-CurrentPosition=$START_POSITION
-if [[ $Random -eq 1 ]]
-then
-	CurrentPosition=$START_POSITION
-elif [[ $Random -eq 2 ]]
-then
-	CurrentPosition=$(( $CurrentPosition+$Number ))
-elif [[ $Random -eq 3 ]]
-then
-	CurrentPosition=$(( $CurrentPosition-$Number ))
-else
+while [[ position -le 100 ]]
+do
+	Random=$(( RANDOM%3 + 1 ))
+	currentposition=$START_POSITION
+	if [[ $Random -eq 1 ]]
+	then
+		CurrentPosition=$currentposition
+	elif [[ $Random -eq 2 ]]
+	then
+		CurrentPosition=$(( $currentposition+$Number ))
+	elif [[ $Random -eq 3 ]]
+	then
+		CurrentPosition=$(( $currentposition-$Number ))
+	else
 	echo "wrong input"
-fi
+	fi
 done
-
+echo $CurrentPosition
 
